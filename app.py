af366e9fbbd1cabbaa064f9df68e252ea5ee4498
tangx <tangxinzhi158@hotmail.com>
2021-01-04 00:41:32 +0100

    add downloader testing method

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+    # return render_template('index_backup.html')
 
 
 @app.route('/convert', methods=['POST'])

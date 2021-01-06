682954f81b8c16e157f0c27b94c2634d31908921
tangx <tangxinzhi158@hotmail.com>
2021-01-06 19:56:14 +0100

    add download button in each card

--- angular-flask/src/app/app.module.ts
+++ angular-flask/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {MatCardModule} from "@angular/material/card";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import {FlexLayoutModule} from "@angular/flex-layout";
     MatProgressBarModule,
     MatToolbarModule,
     FlexLayoutModule,
+    MatIconModule,
   ],
   providers: [HttpClientModule,],
   bootstrap: [AppComponent]

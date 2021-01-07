3f5d024f5a1b1a9f33cd88df08285e3923e787bc
tangx <tangxinzhi158@hotmail.com>
2021-01-07 15:36:02 +0100

    improve the dialog popup

--- youtube_api.py
+++ youtube_api.py
@@ -8,7 +8,7 @@ class YoutubeApi:
     def __init__(self):
         self.youtube = build('youtube', 'v3', developerKey=google_youtube_api_key)
 
-    def search(self, key_word, target_type='video', max_len=20):
+    def search(self, key_word, target_type='video', max_len=16):
         req = self.youtube.search().list(q=key_word, part='snippet', type=target_type, maxResults=max_len)
         return req.execute()
 

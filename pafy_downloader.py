5fe0790e150718e26ff736ba137c36623cabf371
tangx <tangxinzhi158@hotmail.com>
2021-01-11 10:49:18 +0100

    refactor string formatting in python

--- pafy_downloader.py
+++ pafy_downloader.py
@@ -13,13 +13,15 @@ def get_download_types(youtube_link):
 
     ans_formats = []
     for temp in streams:
-        temp_format = {'format_name': str(temp.extension).replace(" ", "") + " " + str(temp.notes).replace(" ", ""),
-                       'format_url': temp.url}
+        temp_format = {
+            'format_name': "{} {}".format(str(temp.extension).replace(" ", ""), str(temp.notes).replace(" ", "")),
+            'format_url': temp.url}
         ans_formats.append(temp_format)
 
     for temp in audio_streams:
-        temp_format = {'format_name': str(temp.extension).replace(" ", "") + " " + str(temp.quality).replace(" ", ""),
-                       'format_url': temp.url}
+        temp_format = {
+            'format_name': "{} {}".format(str(temp.extension).replace(" ", ""), str(temp.quality).replace(" ", "")),
+            'format_url': temp.url}
         ans_formats.append(temp_format)
 
     return ans_formats
--- pytube_downloader.py
+++ pytube_downloader.py
@@ -22,8 +22,8 @@ def get_download_types(youtube_link):
     for supported in SUPPORTED_TYPES:
         temp = streams.filter(subtype=supported).order_by('resolution').desc()
         if temp:
-            ans_formats.append(
-                str(temp.first().mime_type).replace(" ", "") + " " + str(temp.first().resolution)).replace(" ", "")
+            ans_formats.append("{} {}".format(str(temp.first().mime_type).replace(" ", ""),
+                                              str(temp.first().resolution)).replace(" ", ""))
 
     return ans_formats
 
@@ -33,12 +33,12 @@ def get_response_pytube(youtube_link):
     print(youtube_link)
     print("-------------------------------------------")
 
-    file_name = PREFIX_NAME + str(uuid.uuid4())
+    file_name = f'{PREFIX_NAME}{str(uuid.uuid4())}'
     req = requests.get(youtube_link, stream=True)
     resp = Response(stream_with_context(req.iter_content(chunk_size=CHUNK_SIZE, decode_unicode=False)),
                     mimetype=MIME_TYPE,
                     content_type=req.headers['content-type'],
                     direct_passthrough=True)
-    resp.headers['Content-Disposition'] = 'attachment;filename=' + file_name + SUBTYPE_NAME_PAIR[1]
+    resp.headers['Content-Disposition'] = f'attachment;filename={file_name}{SUBTYPE_NAME_PAIR[1]}'
 
     return resp
